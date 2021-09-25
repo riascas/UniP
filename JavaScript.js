9c65818fc58144fae5f52f4e9dddffa9673bf654
facu-sergio <46017478+facu-sergio@users.noreply.github.com>
2021-09-25 18:53:34 -0300

    Se agregan pantallas perfil alumno y formulario perfil alumno

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,16 @@
 $(function() { $('.selectpicker').selectpicker(); });
+
+//funcion para previsualizar imagen en pantalla formulario alumno
+document.getElementById('file').onchange=function(e){
+    let reader=new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload=function(){
+        let preview=document.getElementById('preview');
+            image=document.createElement('img');
+            image.src=reader.result;
+            image.style.width="240px";
+            image.className +="rounded";
+            preview.innerHTML='';
+            preview.append(image);
+    }
+}
