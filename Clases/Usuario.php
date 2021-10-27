75aac9a70b1c122f71d4ce00d49013fb79ca60ce
Diego Romero <dhernan.romero@gmail.com>
2021-10-27 20:07:05 -0300

    clase usuarios

--- Clases/Usuario.php
+++ Clases/Usuario.php
@@ -20,6 +20,56 @@ class Usuario{
     {
         return $this->IdUsuario;
     }
+
+    public function setNombre($nombre)
+    {
+        $this->Nombre = $nombre;
+    }
+
+    public function getNombre()
+    {
+        return $this->Nombre;
+    }
+
+    public function setApellido($apellido)
+    {
+        $this->Apellido = $apellido;
+    }
+
+    public function getApellido()
+    {
+        return $this->Apellido;
+    }
+
+    public function setFechaNacimiento($fecNac)
+    {
+        $this->FechaNacimiento = $fecNac;
+    }
+
+    public function getFechaNacimiento()
+    {
+        return $this->FechaNacimiento;
+    }
+
+    public function setEmail($email)
+    {
+        $this->Email = $email;
+    }
+
+    public function getEmail()
+    {
+        return $this->Email;
+    }
+
+    public function setImagenPerfil($imgPerfil)
+    {
+        $this->ImagenPerfil = $imgPerfil;
+    }
+
+    public function getImagenPerfil()
+    {
+        return $this->ImagenPerfil;
+    }
 }
 
 ?>
