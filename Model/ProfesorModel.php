4304a58cccc4e0e954360a1e8626ca2d6eb5fa36
SofiaMartinez-Ramovecchi <zunidaramivecchi@gmail.com>
2021-11-05 13:33:18 -0300

    edito model profesor

--- Model/ProfesorModel.php
+++ Model/ProfesorModel.php
@@ -1,25 +1,36 @@
-<?php
-    class ProfesorModel extends ConexionDB
-    {
-        public function Listar(){
-            $this->query = "SELECT P.IdProfesor,U.IdUsuario, U.Nombre, U.Apellido,U.Cuil, R.nombre, R.IdMaestro, R.IdProfesor,R.Materia
-                            FROM Profesor P
-                            INNER JOIN Usuario U ON U.IdUsuario = P.IdUsuario";
-            $this->obtenerRows(array(
-                ':inscripcionId' => $this->inscripcionId
-            ));
-            return $this->rows;
-        }
+<?php 
 
-        public function Guardar(){
-            $this->query = "INSERT INTO Profesor(IdProfesor, IdUsuario, URL)
-                            VALUES(:nombre, :url)";
-            $this->ejecutar( array(
-                    ':nombre' => $this->nombre,
-                    ':url' => $this->url
-                ));
-            $this->documentoId = $this->ultimoId();
-        }
-    }
+require_once('./Clases/Persona.php');
+require_once('./Model/ConexionDB.php');   
 
+    class AlumnoModel{
+        private $servername = "localhost";
+        private $dbname = "unip";
+        private $username = "root";
+        private $password = "";
+
+
+       public function guardar(Persona $usr) {
+        try{
+
+            /*me conecto a la BD*/
+            $conexion = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password,
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            /*query para insertar datos en tabla persona*/
+            $sentenciaSQL= $conexion->prepare("INSERT INTO persona (Nombre, Apellido, DNI, Email, FechaNacimiento, NombreCalle, NumeroCalle, Password, IdLocalidad)
+            VALUES (:Nombre, :Apellido, :DNI,:Email, :FechaNacimiento,  :NombreCalle, :NumeroCalle, Password, :IdLocalidad);"); 
+            /* valores para los parametros */
+            $sentenciaSQL->bindParam('Nombre',$usr->getNombre());
+            $sentenciaSQL->bindParam('Apellido',$usr->getApellido());
+            $sentenciaSQL->bindParam('DNI',$usr->getDNI());
+            $sentenciaSQL->bindParam('Email',$usr->getEmail());
+            $sentenciaSQL->bindParam('NombreCalle',$usr->getCalle());
+            $sentenciaSQL->bindParam('NumeroCalle',$usr->getNumeroCalle());
+            $sentenciaSQL->bindParam('IdLocalidad',$usr->getLocalidad());
+            $sentenciaSQL->execute();
+        }catch( Exception $ex){
+            echo $ex->getMessage();
+        }
+    } 
+} 
 ?>
