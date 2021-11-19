9c8fef7bff4f496784789430f59a0b220853b8c8
riascas <ruth.iascas@gmail.com>
2021-11-18 22:24:41 -0300

    Cambios varios -Front vista perfil depto alumnos -Vista peril administrador -Clase DeptoAlumnos -Clase Administrador

---
+++ Clases/Administrador.php
@@ -0,0 +1,33 @@
+<?php
+require_once('Persona.php');
+class Administrador extends Persona
+{
+    private $IdDepAlumno;
+    private $IdUsuario;
+    private $Cuil;
+
+    public function __construct($nombre, $apellido,$dni,$nacionalidad,$fecNac,$telefono,$estadoCivil,$provincia,$localidad,$calle,$numeroCalle,$email,$imgPerfil,$pass,$idprovincia,$idLocalidad)
+    {   
+        parent::__construct($nombre, $apellido,$dni,$nacionalidad,$fecNac,$telefono,$estadoCivil,$provincia,$localidad,$calle,$numeroCalle,$email,$imgPerfil);  
+        $this->Password = $pass;
+        $this->IdRol = 1;
+    }
+
+    public function setPassword($pass)
+    {
+        $this->Password = $pass;
+    }
+    
+    public function getPassword()
+    {
+        return $this->Password; 
+    }
+
+    
+    public function getRol()
+    {
+        return $this->IdRol;
+    }
+}
+
+?>
