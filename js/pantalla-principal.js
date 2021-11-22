b2cca687358f4a29188eee7a47694a72b1201f40
facu-sergio <46017478+facu-sergio@users.noreply.github.com>
2021-11-22 16:49:41 -0300

    Se soluciona bug con las ventanas modal de login en la pantalla principal

---
+++ js/pantalla-principal.js
@@ -0,0 +1,18 @@
+
+$('#btnDptoAlumnos').click(function(){
+    $("#loginmodal3").modal("show");
+})
+
+$('#btnAlumno').click(function(){
+    $("#loginmodal").modal("show");
+})
+
+
+$('#btnProfesores').click(function(){
+    $("#loginmodal3").modal("show");
+})
+
+
+$('#btnReclutadores').click(function(){
+    $("#loginmodal2").modal("show");
+})
--- pantalla-principal.php
+++ pantalla-principal.php
@@ -11,12 +11,16 @@ include('./login-google.php');
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!--bootstrap-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
   <link rel="stylesheet" href="css/estilo.css">
   <link rel="stylesheet" href="./fontawesome/css/all.css">
   <title>Pantalla principal </title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 </head>
 
 <body>
@@ -24,8 +28,7 @@ include('./login-google.php');
     <div class="container-fluid">
       <a class="navbar-brand text-white" href="#">Unip</a>
       <img src="./img/Logo3.png" alt="5%" width="3%">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -64,18 +67,15 @@ include('./login-google.php');
     <div class="row   buscador">
       <div class="col-md-2"></div>
       <div class="col-md-6 ">
-        <a type="button" class="btn btn-primary btn-lg" href="./v.principal-reclutador.html"
-          >Candidatos</a>
-        <a type="button" class="btn btn-success btn-lg text-white" href="./V.principal-alumno.php"
-          >Publicaciones</a>
+        <a type="button" class="btn btn-primary btn-lg" href="./v.principal-reclutador.html">Candidatos</a>
+        <a type="button" class="btn btn-success btn-lg text-white" href="./V.principal-alumno.php">Publicaciones</a>
       </div>
     </div>
 
     <div class="row ">
       <div class="col-md-2"></div>
       <div class="col-md-6 ">
-        <input class="form-control mr-sm-2 bg-i shadow border-dark mt-1 " type="search" placeholder="Buscar "
-          aria-label="Search">
+        <input class="form-control mr-sm-2 bg-i shadow border-dark mt-1 " type="search" placeholder="Buscar " aria-label="Search">
       </div>
 
       <div class="row filaTarjetas">
@@ -89,14 +89,14 @@ include('./login-google.php');
                 </div>
                 <div class="row mt-1">
                   <div class="col-sm-12">
-                    <a class="btn btn-primary w-100 my-1" data-bs-toggle="modal" data-bs-target="#loginmodal" >
+                    <button class="btn btn-primary w-100 my-1" id="btnAlumno">
                       <div class="row aling-items-center">
-                          <div class="col-1"></div>
-                          <div class="col-11">
-                              <p class=" h5 ps-5 text-center">INGRESAR</p> 
-                          </div>
+                        <div class="col-1"></div>
+                        <div class="col-11">
+                          <p class=" h5 ps-5 text-center">INGRESAR</p>
+                        </div>
                       </div>
-                    </a>
+                    </button>
                   </div>
                 </div>
               </div>
@@ -114,14 +114,14 @@ include('./login-google.php');
                 </div>
                 <div class="row mt-3">
                   <div class="col-sm-12">
-                    <a class="btn btn-primary w-100 my-1" data-bs-toggle="modal" data-bs-target="#loginmodal2">
+                    <button id="btnReclutadores" class="btn btn-primary w-100 my-1" data-bs-toggle="modal" data-bs-target="#loginmodal2">
                       <div class="row aling-items-center">
-                          <div class="col-1"></div>
-                          <div class="col-11">
-                              <p class=" h5 ps-5 text-center">INGRESAR</p> 
-                          </div>
+                        <div class="col-1"></div>
+                        <div class="col-11">
+                          <p class=" h5 ps-5 text-center">INGRESAR</p>
+                        </div>
                       </div>
-                    </a>
+                    </button>
                   </div>
                 </div>
               </div>
@@ -138,24 +138,24 @@ include('./login-google.php');
                 </div>
                 <div class="row mt-1">
                   <div class="col-sm-12">
-                    <a class="btn btn-primary w-100 my-1" data-bs-toggle="modal" data-bs-target="#loginmodal4" >
+                    <button id="btnProfesores" class="btn btn-primary w-100 my-1" >
                       <div class="row aling-items-center">
-                          <div class="col-1"></div>
-                          <div class="col-11">
-                              <p class=" h5 ps-5 text-center">INGRESAR</p> 
-                          </div>
+                        <div class="col-1"></div>
+                        <div class="col-11">
+                          <p class=" h5 ps-5 text-center">INGRESAR</p>
+                        </div>
                       </div>
-                    </a>
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        
+
         <div class="col-md-3">
           <div class="card tarjeta-inicio border border-2 border-secondary tarjetas">
-            <img src="img/departamento-alumnos.jpg" class="card-img-top" >
+            <img src="img/departamento-alumnos.jpg" class="card-img-top">
             <div class="card-body">
               <div class="container">
                 <div class="row">
@@ -163,13 +163,13 @@ include('./login-google.php');
                 </div>
                 <div class="row mt-1">
                   <div class="col-sm-12">
-                    <a class="btn btn-primary w-100 my-1" data-bs-toggle="modal" data-bs-target="#loginmodal3" >
+                    <button class="btn btn-primary w-100 my-1" id="btnDptoAlumnos" name="btnDptoAlumnos">
                       <div class="row aling-items-center">
-                          <div class="col-1"></div>
-                          <div class="col-11">
-                              <p class=" h5 ps-5 text-center">INGRESAR</p> 
-                          </div>
-                      </div>
+                        <div class="col-1"></div>
+                        <div class="col-11">
+                          <p class=" h5 ps-5 text-center">INGRESAR</p>
+                        </div>
+                    </button>
                     </a>
                   </div>
                 </div>
@@ -178,23 +178,25 @@ include('./login-google.php');
           </div>
         </div>
       </div>
-      <div class="modal fade" role="dialog" id="loginmodal" >
+      <!----modal para login Alumnos------>
+      <div class="modal fade" role="dialog" id="loginmodal">
         <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header bg-secondary">
-                  <h3 class="modal-tittle text-white ">Bienvenido</h3>
-                </div>
-                <div class="modal-body">
-                  <div class="row text-center">
-                    <div class="col-12 h3">Iniciar sesión</div>
-                </div>
-                <div class="modal-footer">
-                  <?php echo $login_button ;?>
-                </div>
+          <div class="modal-content">
+            <div class="modal-header bg-secondary">
+              <h3 class="modal-tittle text-white ">Bienvenido</h3>
             </div>
+            <div class="modal-body">
+              <div class="row text-center">
+                <div class="col-12 h3">Iniciar sesión</div>
+              </div>
+              <div class="modal-footer">
+                <?php echo $login_button; ?>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
+      <!----modal para login reclutadores ------>
       <div class="modal fade" role="dialog" id="loginmodal2">
         <div class="modal-dialog">
           <div class="modal-content">
@@ -210,29 +212,29 @@ include('./login-google.php');
                 <div class="col-sm-10">
                   <input type="text" readonly class="form-control-plaintext border border-secondary rounded" id="staticEmail" value="email@example.com">
                 </div>
-            </div>
-            <div class="mb-3 row">
-              <label for="inputPassword" class="col-sm-2 col-form-label ">Password</label>
-              <div class="col-sm-10">
-                <input type="password" class="form-control-plaintext border border-secondary rounded" id="inputPassword">
               </div>
+              <div class="mb-3 row">
+                <label for="inputPassword" class="col-sm-2 col-form-label ">Password</label>
+                <div class="col-sm-10">
+                  <input type="password" class="form-control-plaintext border border-secondary rounded" id="inputPassword">
+                </div>
+              </div>
+              <a type="submit" class="btn btn-danger" href="./v.principal-reclutador.html">
+                Ingresar
+              </a>
+              <a type="button" class="btn btn-link" href="./recuperar-contraseña.html">
+                Olvide mi contraseña
+              </a>
+              <a href="./FormularioCargaReclutador.html" class="btn btn-link">
+                No tengo cuenta
+              </a>
             </div>
-            <a type="submit" class="btn btn-danger" href="./v.principal-reclutador.html">
-              Ingresar
-            </a>
-        <a type="button" class="btn btn-link" href="./recuperar-contraseña.html" >
-              Olvide mi contraseña 
-        </a>
-        <a  href="./FormularioCargaReclutador.html" class="btn btn-link">
-          No tengo cuenta
-        </a>
-          </div>
           </div>
         </div>
       </div>
 
-
-      <div class="modal fade" role="dialog" id="loginmodal3" >
+      <!----modal para login dptAlumno y profesores------>
+      <div class="modal fade" role="dialog" id="loginmodal3">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header bg-secondary">
@@ -247,66 +249,26 @@ include('./login-google.php');
                 <div class="col-sm-10">
                   <input type="text" readonly class="form-control-plaintext border border-secondary rounded" id="staticEmail" value="email@example.com">
                 </div>
-            </div>
-            <div class="mb-3 row">
-              <label for="inputPassword" class="col-sm-2 col-form-label ">Password</label>
-              <div class="col-sm-10">
-                <input type="password" class="form-control-plaintext border border-secondary rounded" id="inputPassword">
-              </div>
-            </div>
-            <a type="submit" class="btn btn-danger" href="./Rutas/ListarReclutador.php">
-              Ingresar
-            </a>
-        <a type="button" class="btn btn-link" href="./recuperar-contraseña.html" >
-              Olvide mi contraseña 
-        </a>
-        <a  href="./FormularioCargaReclutador.html" class="btn btn-link">
-          No tengo cuenta
-        </a>
-          </div>
-          </div>
-        </div>
-      </div>
-      <!----modal para profesores------>
-      <div class="modal fade" role="dialog" id="loginmodal4" >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header bg-secondary">
-              <h3 class="moddal-tittle text-white ">Bienvenido</h3>
-            </div>
-            <div class="modal-body">
-              <div class="row text-center">
-                <div class="col-12 h3">Iniciar Sesión</div>
               </div>
               <div class="mb-3 row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                <label for="inputPassword" class="col-sm-2 col-form-label ">Password</label>
                 <div class="col-sm-10">
-                  <input type="text" readonly class="form-control-plaintext border border-secondary rounded" id="staticEmail" value="email@example.com">
+                  <input type="password" class="form-control-plaintext border border-secondary rounded" id="inputPassword">
                 </div>
-            </div>
-            <div class="mb-3 row">
-              <label for="inputPassword" class="col-sm-2 col-form-label ">Password</label>
-              <div class="col-sm-10">
-                <input type="password" class="form-control-plaintext border border-secondary rounded" id="inputPassword">
               </div>
+              <a type="submit" class="btn btn-danger" href="./Rutas/ListarReclutador.php">
+                Ingresar
+              </a>
+              <a type="button" class="btn btn-link" href="./recuperar-contraseña.html">
+                Olvide mi contraseña
+              </a>
             </div>
-            <a type="submit" class="btn btn-danger" href="./v.principal-reclutador.html">
-              Ingresar
-            </a>
-        <a type="button" class="btn btn-link" href="./recuperar-contraseña.html" >
-              Olvide mi contraseña 
-        </a>
-        <a  href="./perfil-alumno.html" class="btn btn-link">
-          No tengo cuenta
-        </a>
-          </div>
           </div>
         </div>
       </div>
-    
 
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </div>
+    <script src="./js/pantalla-principal.js"></script>
 </body>
 
 </html>
